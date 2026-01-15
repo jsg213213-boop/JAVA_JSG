ba0f343a99a4ce1c835ddb493a93019e6779ddde
정성규 <jsg213213@naver.com>
2026-01-15 17:25:20 +0900

    260115 정성규

--- src/Class_260115/_1_Hyndaicar.java
+++ src/Class_260115/_1_Hyndaicar.java
@@ -23,29 +23,29 @@ class Car {
     class _1_Hyundaicar {
         public static void main(String[] args) {
             // Car 객체 생성
-            Car car1 = new Car("스타리아", 2026, "MPV(다목적 차량)");
-            Car car2 = new Car("코나", 2026, "소형 SUV");
-            Car car3 = new Car("팰리세이드", 2026, "대형 SUV");
-            Car car4 = new Car("투싼", 2026, "준중형 SUV");
-            Car car5 = new Car("싼타페", 2026, "중형 SUV");
-            Car car6 = new Car("아이오닉5", 2026, "준중형 SUV (전기차)");
-            Car car7 = new Car("아이오닉6", 2026, "중형 세단 (전기차)");
-            Car car8 = new Car("제네시스G80", 2026, "대형 세단");
-            Car car9 = new Car("베뉴", 2026, "소형 SUV");
+            Car car01 = new Car("스타리아", 2026, "승합차");
+            Car car02 = new Car("코나", 2026, "소형 SUV");
+            Car car03 = new Car("팰리세이드", 2026, "대형 SUV");
+            Car car04 = new Car("투싼", 2026, "준중형 SUV");
+            Car car05 = new Car("싼타페", 2026, "중형 SUV");
+            Car car06 = new Car("아이오닉5", 2026, "준중형 SUV (전기차)");
+            Car car07 = new Car("아이오닉6", 2026, "중형 세단 (전기차)");
+            Car car08 = new Car("제네시스G80", 2026, "대형 세단");
+            Car car09 = new Car("베뉴", 2026, "소형 SUV");
             Car car10 = new Car("캐스퍼", 2026, "경형 SUV");
             Car car11 = new Car("그랜저", 2026, "대형 세단");
             Car car12 = new Car("아반떼", 2026, "준중형 세단");
             Car car13 = new Car("쏘나타", 2026, "중형 세단");
 
-            car1.introduceCar();
-            car2.introduceCar();
-            car3.introduceCar();
-            car4.introduceCar();
-            car5.introduceCar();
-            car6.introduceCar();
-            car7.introduceCar();
-            car8.introduceCar();
-            car9.introduceCar();
+            car01.introduceCar();
+            car02.introduceCar();
+            car03.introduceCar();
+            car04.introduceCar();
+            car05.introduceCar();
+            car06.introduceCar();
+            car07.introduceCar();
+            car08.introduceCar();
+            car09.introduceCar();
             car10.introduceCar();
             car11.introduceCar();
             car12.introduceCar();
