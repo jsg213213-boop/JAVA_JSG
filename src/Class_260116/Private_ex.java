129e8e86474361288fada5ae81bc97e63c075a44
정성규 <jsg213213@naver.com>
2026-01-16 17:03:41 +0900

    260116 정성규

--- src/Class_260116/Private_ex.java
+++ src/Class_260116/Private_ex.java
@@ -2,12 +2,9 @@ package Class_260116;
 
 public class Private_ex {
     private String name;
-
-    // 그래서, 세터를 메서드를 이용해서, 값 설정.
     public void setName(String n) {
         name = n;
     }
-    // 게터 메서드를 이용해서, 값 조회.
     public String getName() {
         return name;
     }
