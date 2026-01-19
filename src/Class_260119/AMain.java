a14e8928b57f2dced6b31e40a98e9ebb84268c0c
정성규 <jsg213213@naver.com>
2026-01-19 17:03:45 +0900

    260119 정성규

--- src/Class_260119/AMain.java
+++ src/Class_260119/AMain.java
@@ -29,19 +29,16 @@ public class AMain {
         System.out.println();
 
         System.out.println("=== 업캐스팅과 오버라이딩 ===");
-        // 업캐스팅: 부모 타입 A 변수에 자식 객체 B를 할당
+
         B upcastedA = new B();
-        // 업캐스팅 상태여도 실제 객체인 B의 오버라이딩된 메서드가 호출됨
+
         upcastedA.show();
         System.out.println();
 
         System.out.println("=== instanceof 검사 및 다운캐스팅 ===");
-        // 현재 upcastedA는 A 타입이지만 실제 인스턴스는 B임
+
         System.out.println("검사 결과: upcastedA는 B의 인스턴스가 맞습니다.");
 
-        // 다운캐스팅: 부모 타입을 다시 자식 타입으로 강제 변환
-
-        // 이제 B 클래스에만 있는 메서드에 접근 가능
         upcastedA.onlyB();
     }
 }
--- src/Class_260119/Partent.java
+++ src/Class_260119/Partent.java
@@ -1,14 +1,5 @@
 package Class_260119;
 
-class Parent {
-    Parent() {
-        System.out.println("부모 생성자 호출");
-    }
-}
-
-class Child extends Parent {
-}
-
 class Main {
     public static void main(String[] args) {
         System.out.println("--- 객체 생성 시작 ---");
