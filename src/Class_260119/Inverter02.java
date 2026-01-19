4de49083962fbbe9ffdbddc85afb9b92e26bd4d0
정성규 <jsg213213@naver.com>
2026-01-19 17:09:39 +0900

    260119 정성규

--- src/Class_260119/Inverter02.java
+++ src/Class_260119/Inverter02.java
@@ -1,20 +1,16 @@
 package Class_260119;
 
 interface Camera {
-    void takePhoto();
 }
 
 interface Phone {
-    void makeCall();
 }
 
 class SmartPhone implements Camera, Phone {
-    @Override
     public void takePhoto() {
         System.out.println("사진을 찍습니다. (SmartPhone)");
     }
 
-    @Override
     public void makeCall() {
         System.out.println("전화를 겁니다. (SmartPhone)");
     }
@@ -27,9 +23,7 @@ public class Inverter02 {
         myPhone.takePhoto();
         myPhone.makeCall();
 
-        // 다형성을 활용한 참조
-        Camera cameraRef = myPhone;
-        cameraRef.takePhoto(); // 가능
-        // cameraRef.makeCall(); // 불가능 (Camera 인터페이스에는 makeCall이 없음)
+        myPhone.takePhoto(); // 가능
+        // 불가능 (Camera 인터페이스에는 makeCall이 없음)
     }
 }
