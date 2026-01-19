8083b5765db14e5b0d59334e927c53908692520b
정성규 <jsg213213@naver.com>
2026-01-19 17:05:09 +0900

    260119 정성규

--- src/Class_260119/Inverter.java
+++ src/Class_260119/Inverter.java
@@ -1,43 +1,42 @@
 package Class_260119;
 
 interface RemoteControl {
-    void turnOn();  // 추상 메서드 (구현부가 없음)
-    void turnOff(); // 추가 예시 메서드
-}
+    void turnOn();
 
-class TV implements RemoteControl {
-    @Override
-    public void turnOn() {
-        System.out.println("TV를 켭니다.");
+    void turnOff();
+
+    class TV implements RemoteControl {
+        @Override
+        public void turnOn() {
+            System.out.println("TV를 켭니다.");
+        }
+
+        public void turnOff() {
+            System.out.println("TV를 끕니다.");
+        }
     }
 
-    @Override
-    public void turnOff() {
-        System.out.println("TV를 끕니다.");
+    class Radio implements RemoteControl {
+        @Override
+        public void turnOn() {
+            System.out.println("라디오를 켭니다.");
+        }
+
+        @Override
+        public void turnOff() {
+            System.out.println("라디오를 끕니다.");
+        }
     }
-}
 
-// 3. 또 다른 구현 클래스 (확장성 예시)
-class Radio implements RemoteControl {
-    @Override
-    public void turnOn() {
-        System.out.println("라디오를 켭니다.");
-    }
+    class Inverter {
+        public static void main(String[] args) {
 
-    @Override
-    public void turnOff() {
-        System.out.println("라디오를 끕니다.");
-    }
-}
+            RemoteControl myTV = new TV();
+            RemoteControl myRadio = new Radio();
 
-public class Inverter {
-    public static void main(String[] args) {
-
-        RemoteControl myTV = new TV();
-        RemoteControl myRadio = new Radio();
-
-        myTV.turnOn();
-        myRadio.turnOn();
-        myTV.turnOff();
+            myTV.turnOn();
+            myRadio.turnOn();
+            myTV.turnOff();
+        }
     }
 }
